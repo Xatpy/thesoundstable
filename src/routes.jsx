745a7af689e453c3e9f273ef710f38c8d0a01204
Jaime <jaime.chapinal@gmail.com>
2022-12-28 18:49:41 +0100

    Add sitemap generator

---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Switch, Route } from "react-router";
+
+import { BrowserRouter } from "react-router-dom";
+
+export default (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" />
+      <Route path={`/ElXokas`} />
+      <Route path={`/xokas`} />
+      <Route path={`/Ibai`} />
+      <Route path={`/LuisEnrique`} />
+      <Route path={`/IlloJuan`} />
+      <Route path={`/LaVidaModerna`} />
+      <Route path={`/Maldini`} />
+      <Route path={`/Rubius`} />
+      <Route path="*" />
+    </Switch>
+  </BrowserRouter>
+);

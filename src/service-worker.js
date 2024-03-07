8790ae052f44f0001bfef81b006dfa2e53c643fa
Jaime <jaime.chapinal@gmail.com>
2024-03-07 22:26:51 +0100

    Bump

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,7 @@ import { precacheAndRoute } from "workbox-precaching";
 // eslint-disable-next-line no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);
 
-const dynamicCacheName = "0.0.14";
+const dynamicCacheName = "0.0.15";
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("install", (event) => {

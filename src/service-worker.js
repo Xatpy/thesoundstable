946b65faf3a2582c2bf1ab1f80d44b64c59f3d8d
Jaime <jaime.chapinal@gmail.com>
2024-03-07 12:58:58 +0100

    Fix update

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,7 @@ import { precacheAndRoute } from "workbox-precaching";
 // eslint-disable-next-line no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);
 
-const dynamicCacheName = "0.0.9";
+const dynamicCacheName = "0.0.10";
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("install", (event) => {
